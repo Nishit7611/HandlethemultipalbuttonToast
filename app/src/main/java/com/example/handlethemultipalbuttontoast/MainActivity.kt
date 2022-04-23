6566639a0fe0c88ccf7b453c6facb9aa4b73f734
Nishit Patel <Nishitp9426@gmail.com>
2022-04-23 15:32:53 +0530

    Commit

--- app/src/main/java/com/example/handlethemultipalbuttontoast/MainActivity.kt
+++ app/src/main/java/com/example/handlethemultipalbuttontoast/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.handlethemultipalbuttontoast
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        idtxt.setOnClickListener {
+            var cal = Calendar.getInstance()
+            var year = cal.get(Calendar.YEAR)
+            var month = cal.get(Calendar.MONTH)
+            var day = cal.get(Calendar.DAY_OF_MONTH)
+            var datePicker = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { datePicker, year,month,day ->
+                  idtxt.setText(  "This is a Date You Select " + " $year/${month + 1}/ $day")
+                },year,month,day ).show()
+
+
+
+
+        }
+
     }
     fun onClick(view: android.view.View ){
         if (view.id == R.id.btn1){
@@ -37,5 +58,29 @@ class MainActivity : AppCompatActivity() {
         else if(view.id == R.id.btn2){
             Toast.makeText(this,"This is a Button 2",Toast.LENGTH_LONG).show()
         }
+        else if (view.id == R.id.btn3 )
+        {
+            var cal = Calendar.getInstance()
+            var year = cal.get(Calendar.YEAR)
+            var month = cal.get(Calendar.MONTH)
+            var day = cal.get(Calendar.DAY_OF_MONTH)
+            var datePicker = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { datePicker, year,month,day ->
+                    Toast.makeText(this,"This is a Date You Select " + " $year/${month + 1}/ $day",Toast.LENGTH_LONG ).show()
+                },year,month,day ).show()
+
+
+
+
+        }
+        else if (view.id == R.id.btn4 )
+        {
+            var timePicker = TimePickerDialog(this,null,5,47,false)
+            timePicker.show()
+
+        }
+
     }
+
+
 }
